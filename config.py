d4d1345a432e664e78bdf7e524107233094c7b50
Cougar <cougar@random.ee>
2013-04-03 01:49:37 +0300

    Add configuration module

---
+++ config.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import os
+
+class Config:
+	def __init__(self):
+		self.cfg = {}
+		self.cfg['sql_db_chan'] = 'modbus_channels'
+		self.cfg['sql_db_setup'] = 'asetup'
+		try:
+			import android
+			os.chdir('/sdcard/sl4a/scripts/d4c')
+		except:
+			os.chdir('.')
+
+	def get(self, key):
+		return self.cfg[key] if self.cfg.has_key(key) else ""
+
+if __name__ == '__main__':
+	c = Config();
+	print c.get('sql_db_chan')
+	print c.get('foo')
+
